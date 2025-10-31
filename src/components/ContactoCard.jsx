c22e18ce7e413d19b41e800b859ed3757b0dd85d
Stiven-Hurtado-Valencia <stivenhuv@gmail.com>
2025-10-30 19:58:10 -0500

    ActualizandoA1

--- src/components/ContactoCard.jsx
+++ src/components/ContactoCard.jsx
@@ -1,23 +1,51 @@
-// ContactoCard.jsx
-// Este componente muestra UN contacto de la agenda.
-// Recibe datos (props): nombre, telefono, correo, etiqueta.
+// /* // ContactoCard.jsx
+// // Este componente muestra UN contacto de la agenda.
+// // Recibe datos (props): nombre, telefono, correo, etiqueta.
 
-export default function ContactoCard({ nombre, telefono, correo, etiqueta }) {
+// export default function ContactoCard({ nombre, telefono, correo, etiqueta }) {
+//   return (
+//     <div className="card-contacto">
+//       {/* Nombre del contacto en negrita */
+// /*       <h3 className="card-nombre">{nombre}</h3> */
+
+//       {/* Teléfono */}
+// /*       <p className="card-linea">📞 {telefono}</p> */
+
+//       {/* Correo */}
+//      /*  <p className="card-linea">📧 {correo}</p> */
+
+//       {/* Etiqueta adicional, si existe (por ejemplo: "Instructor", "Cliente", "Compañera") */}
+//     /*   {etiqueta && (
+//         <p className="card-etiqueta">{etiqueta}</p>
+//       )}
+//     </div>
+//   );
+// } */
+
+  export default function ContactoCard({
+  id,
+  nombre,
+  telefono,
+  correo,
+  etiqueta,
+  onDelete,
+}) {
   return (
-    <div className="card-contacto">
-      {/* Nombre del contacto en negrita */}
-      <h3 className="card-nombre">{nombre}</h3>
+    <article className="tarjeta-contacto">
+      <h3>{nombre}</h3>
+      {etiqueta && <p className="tag">{etiqueta}</p>}
+      <p>📞 {telefono}</p>
+      {correo && <p>✉️ {correo}</p>}
 
-      {/* Teléfono */}
-      <p className="card-linea">📞 {telefono}</p>
-
-      {/* Correo */}
-      <p className="card-linea">📧 {correo}</p>
-
-      {/* Etiqueta adicional, si existe (por ejemplo: "Instructor", "Cliente", "Compañera") */}
-      {etiqueta && (
-        <p className="card-etiqueta">{etiqueta}</p>
-      )}
-    </div>
+      <div className="acciones">
+        <button
+          type="button"
+          className="btn-eliminar"
+          onClick={() => onDelete(id)}
+        >
+          Eliminar
+        </button>
+      </div>
+    </article>
   );
 }
